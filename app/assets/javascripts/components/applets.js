b56fc53ce4b71ce3f1cbc5a830200cd6f6ca1a4d
ang <shakuganang@gmail.com>
2022-07-31 00:57:30 +1000

    Add loading react applet skeleton for ruby front end use

---
+++ app/assets/javascripts/components/applets.js
@@ -0,0 +1,10 @@
+// All react components will export from here to html.erb files
+
+// import appletName from "../appletDirectory";
+
+export default {
+  // "applet-name": appletName,
+};
+
+// use in html.erb:
+// <%= react_applet('applet-name', data: { ... }) %>
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,20 @@
 module ApplicationHelper
+
+  # Loads react applet from javascripts/components/applets.js
+  def react_applet(applet, classList: "", styles: "", id: "", data: {})
+    classList = ("react-applet-" + applet + " " + classList)
+    applet_id = generate_applet_id
+
+    html = "<noscript>You need to enable JavaScript to run this app.</noscript>"
+    html += "<div id=\"#{id}\" class=\"#{classList}\" style=\"#{styles}\" data-applet-id=\"#{applet_id}\"></div>"
+    html += "<script type=\"application/json\" id=\"react-applet-data-for-#{applet_id}\">#{data.to_json.html_safe}</script>"
+
+    html.html_safe
+  end
+
+  # Provides react applet with a generated id
+  def generate_applet_id
+    charset = Array("A".."Z") + Array("a".."z") + Array(0..9)
+    Array.new(10) { charset.sample }.join
+  end
 end
